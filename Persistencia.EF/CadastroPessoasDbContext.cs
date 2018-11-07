5e6609785b99137dde152df72d2767c195cee564
Bruno Henrique <bruninho.carvalho159@gmail.com>
2018-11-07 17:17:08 -0200

    FirstCommit
    Iniciada a criação dos projetos e a implementação de codFirst utilizando Entity Framework + MySQL

---
+++ Persistencia.EF/CadastroPessoasDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Dominio;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistencia.EF
+{
+    public class CadastroPessoasDbContext : DbContext
+    {
+        public DbSet<Pessoa> Pessoas { get; set; }
+
+        public CadastroPessoasDbContext() : base("CadastroPessoasDbContext")
+        {
+
+        }
+    }
+}
