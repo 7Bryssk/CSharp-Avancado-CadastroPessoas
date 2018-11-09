2a4705e7fe7ad27216e764e3649c56b98fc313fb
Bruno Henrique <bruninho.carvalho159@gmail.com>
2018-11-09 13:46:26 -0200

    Implementação interface WPF
    Implementação de interface Windows Presentation Foundation

---
+++ CadastroPessoas.WPF/CadastrarPessoa.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Dominio;
+using Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CadastroPessoas.WPF
+{
+    /// <summary>
+    /// Lógica interna para CadastrarPessoa.xaml
+    /// </summary>
+    public partial class CadastrarPessoa : Window
+    {
+        public CadastrarPessoa()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            IRepositorio<Pessoa> repositorioPessoa = new PessoaRepositorio();
+            Pessoa pessoa = new Pessoa
+            {
+                nome = txbNome.Text,
+                idade = Convert.ToInt32(txbIdade.Text),
+                endereco = txbEndereco.Text
+            };
+
+            repositorioPessoa.Adicionar(pessoa);
+            Close();
+        }
+    }
+}
---
+++ CadastroPessoas.WPF/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using Dominio;
+using Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CadastroPessoas.WPF
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CarregarDataGrid()
+        {
+            IRepositorio<Pessoa> repositorioPessoas = new PessoaRepositorio();
+            List<Pessoa> listaPessoas = repositorioPessoas.SelecionarTodos();
+            dgrPessoas.ItemsSource = listaPessoas;
+        }
+
+        private void WindowMain_Loaded(object sender, RoutedEventArgs e)
+        {
+            CarregarDataGrid();
+        }
+
+        private void btnAdicionar_Click(object sender, RoutedEventArgs e)
+        {
+            CadastrarPessoa cadastrarPessoa = new CadastrarPessoa();
+            cadastrarPessoa.ShowDialog();
+            CarregarDataGrid();
+        }
+    }
+}
