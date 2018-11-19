318f9f3e723606d976e6daf10f8287311ced9ad8
Bruno Henrique <bruninho.carvalho159@gmail.com>
2018-11-19 13:40:09 -0200

    aula de threads

--- CadastroPessoas/FrmPessoas.cs
+++ CadastroPessoas/FrmPessoas.cs
@@ -7,6 +7,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -14,6 +15,10 @@ namespace CadastroPessoas
 {
     public partial class FrmPessoas : Form
     {
+
+        private List<Pessoa> _pessoas = new List<Pessoa>();
+        private static readonly object locker = new Object();
+
         public FrmPessoas()
         {
             InitializeComponent();
@@ -21,16 +26,64 @@ namespace CadastroPessoas
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            PreencherDataGridView();
+            Thread thread = new Thread(PreencherDataGridView);
+            thread.Start();
+            // Sem thread
+            // PreencherDataGridView
         }
 
         private void PreencherDataGridView()
         {
-            IRepositorio<Pessoa> repositorioPessoas = new PessoaRepositorio();
-            List<Pessoa> pessoas = repositorioPessoas.SelecionarTodos();
 
-            dgvPessoas.DataSource = pessoas;
-            dgvPessoas.Refresh();
+            Thread thread = new Thread(PreencherListaPessoas);
+            Thread thread2 = new Thread(PreencherListaPessoas2);
+            thread2.Start();
+            thread.Start();
+            // Espera a thread anterior terminar a execução para continuar
+            thread.Join();
+            thread2.Join();
+            
+            dgvPessoas.Invoke((MethodInvoker)delegate { dgvPessoas.DataSource = _pessoas; dgvPessoas.Refresh(); });
+
+            // Sem thread
+            //dgvPessoas.DataSource = pessoas;
+            //dgvPessoas.Refresh();
+
+        }
+
+        private void PreencherListaPessoas()
+        {
+            lock (locker) {
+                IRepositorio<Pessoa> repositorioPessoas = new PessoaRepositorio();
+                List<Pessoa> pessoas = repositorioPessoas.SelecionarTodos();
+                foreach (Pessoa p in pessoas)
+                {
+                    p.nome += " T1";
+                    _pessoas.Add(p);
+                }
+            }
+        }
+
+        private void PreencherListaPessoas2()
+        {
+            try
+            {
+                //teste tratamento de erro
+                //throw new Exception("Teste erro!");
+                lock (locker)
+                {
+                    IRepositorio<Pessoa> repositorioPessoas = new PessoaRepositorio();
+                    List<Pessoa> pessoas = repositorioPessoas.SelecionarTodos();
+                    foreach (Pessoa p in pessoas)
+                    {
+                        p.nome += " T2";
+                        _pessoas.Add(p);
+                    }
+                }
+            } catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void btnAdicionarPessoa_Click(object sender, EventArgs e)
